94362d1aa40e47a18c6359654efc56cf33d7842f
Kwangwon Min <kwmin96@gmail.com>
2025-08-21 10:03:01 +0900

    mission 1: method level refactoring

--- mission1/attendance.cpp
+++ mission1/attendance.cpp
@@ -7,134 +7,113 @@
 
 using namespace std;
 
-struct Node {
-	string w;
-	string wk;
-};
+const int MAX_PLAYER = 20;
 
-map<string, int> id1;
+map<string, int> idMap;
+vector<string> removed;
 int id_cnt = 0;
 
-//dat[사용자ID][요일]
-int dat[100][100];
-int points[100];
-int grade[100];
-string names[100];
+int points[MAX_PLAYER];
+int grade[MAX_PLAYER];
+string names[MAX_PLAYER];
+int numAttendOnWed[MAX_PLAYER];
+int numAttendOnWeeken[MAX_PLAYER];
 
-int wed[100];
-int weeken[100];
+void checkNameAndRegisterId(string name)
+{
+	if (idMap.count(name) == 0) {
+		idMap.insert({ name, ++id_cnt });
 
-void input2(string w, string wk) {
-	//ID 부여
-	if (id1.count(w) == 0) {
-		id1.insert({ w, ++id_cnt });
-
-		if (w == "Daisy") {
-			int debug = 1;
-		}
-
-		names[id_cnt] = w;
+		names[id_cnt] = name;
 	}
-	int id2 = id1[w];
-
-	//디버깅용
-	if (w == "Daisy") {
-		int debug = 1;
-	}
-
-
-	int add_point = 0;
-	int index = 0;
-	if (wk == "monday") {
-		index = 0;
-		add_point++;
-	}
-	if (wk == "tuesday") {
-		index = 1;
-		add_point++;
-	}
-	if (wk == "wednesday") {
-		index = 2;
-		add_point += 3;
-		wed[id2] += 1;
-	}
-	if (wk == "thursday") {
-		index = 3;
-		add_point++;
-	}
-	if (wk == "friday") {
-		index = 4;
-		add_point++;
-	}
-	if (wk == "saturday") {
-		index = 5;
-		add_point += 2;
-		weeken[id2] += 1;
-	}
-	if (wk == "sunday") {
-		index = 6;
-		add_point += 2;
-		weeken[id2] += 1;
-	}
-
-	//사용자ID별 요일 데이터에 1씩 증가
-	dat[id2][index] += 1;
-	points[id2] += add_point;
 }
 
-void input() {
+void getAttendanceInfo(string name, string weekday) {
+	//ID 부여
+	checkNameAndRegisterId(name);
+
+	int id = idMap[name];
+	if (weekday == "wednesday") {
+		points[id] += 3;
+		numAttendOnWed[id] += 1;
+	}
+	else if (weekday == "saturday" || weekday == "sunday") {
+		points[id] += 2;
+		numAttendOnWeeken[id] += 1;
+	}
+	else
+		points[id] += 1;
+}
+
+void getRemovedPlayer(int id)
+{
+	if (grade[id] != 1 && grade[id] != 2 && numAttendOnWed[id] == 0 && numAttendOnWeeken[id] == 0) {
+		removed.push_back(names[id]);
+	}
+}
+
+void printGrade(int id)
+{
+	cout << "NAME : " << names[id] << ", ";
+	cout << "POINT : " << points[id] << ", ";
+	cout << "GRADE : ";
+
+	if (grade[id] == 1) {
+		cout << "GOLD" << "\n";
+	}
+	else if (grade[id] == 2) {
+		cout << "SILVER" << "\n";
+	}
+	else {
+		cout << "NORMAL" << "\n";
+	}
+}
+
+void getPoints(int i)
+{
+	if (numAttendOnWed[i] > 9) {
+		points[i] += 10;
+	}
+
+	if (numAttendOnWeeken[i] > 9) {
+		points[i] += 10;
+	}
+}
+
+void getGrade(int i)
+{
+	if (points[i] >= 50) {
+		grade[i] = 1;
+	}
+	else if (points[i] >= 30) {
+		grade[i] = 2;
+	}
+	else {
+		grade[i] = 0;
+	}
+}
+
+void AttendanceManager() {
 	ifstream fin{ "attendance_weekday_500.txt" }; //500개 데이터 입력
 	for (int i = 0; i < 500; i++) {
-		string t1, t2;
-		fin >> t1 >> t2;
-		input2(t1, t2);
+		string name, weekday;
+		fin >> name >> weekday;
+		getAttendanceInfo(name, weekday);
 	}
 
-	for (int i = 1; i <= id_cnt; i++) {
-		if (dat[i][2] > 9) {
-			points[i] += 10;
-		}
-
-		if (dat[i][5] + dat[i][6] > 9) {
-			points[i] += 10;
-		}
-
-		if (points[i] >= 50) {
-			grade[i] = 1;
-		}
-		else if (points[i] >= 30) {
-			grade[i] = 2;
-		}
-		else {
-			grade[i] = 0;
-		}
-
-		cout << "NAME : " << names[i] << ", ";
-		cout << "POINT : " << points[i] << ", ";
-		cout << "GRADE : ";
-
-		if (grade[i] == 1) {
-			cout << "GOLD" << "\n";
-		}
-		else if (grade[i] == 2) {
-			cout << "SILVER" << "\n";
-		}
-		else {
-			cout << "NORMAL" << "\n";
-		}
+	for (int id = 1; id <= id_cnt; id++) {
+		getPoints(id);
+		getGrade(id);
+		getRemovedPlayer(id);
+		printGrade(id);
 	}
-
 	std::cout << "\n";
 	std::cout << "Removed player\n";
 	std::cout << "==============\n";
-	for (int i = 1; i <= id_cnt; i++) {
-
-		if (grade[i] != 1 && grade[i] != 2 && wed[i] == 0 && weeken[i] == 0) {
-			std::cout << names[i] << "\n";
-		}
-	}
+	for (auto name : removed)
+		std::cout << name << "\n";
 }
 
 int main() {
-	input();
+	AttendanceManager();
 }
